2d846ba98ccf036a0e1f55e3a37243d27648e83a
wade k <khufash@gmail.com>
2017-01-07 10:20:40 -0800

    fixed error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { TodoDataService } from './todo-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
   providers: [TodoDataService]
 })
 export class AppComponent {
